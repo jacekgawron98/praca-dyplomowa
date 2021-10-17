d3aab07abedf07a73d822ce4d0e70608e349e5c7
jacekgawron98 <gawron.jacek98@gmail.com>
2021-10-17 21:14:05 +0200

    Add dtos

--- server/src/Services/practiceService.ts
+++ server/src/Services/practiceService.ts
@@ -1,7 +1,19 @@
 import express, { Request, Response } from 'express';
 
-const getItems = (req: Request, res: Response) => {
-    res.status(200).send("Hello world");
+const getItems = (req: Request, res: Response<PracticeItem[]>) => {
+    const items: PracticeItem[] = [
+        {
+            id: "asdfgsdg",
+            name: "item1",
+            repeats: 30
+        },
+        {
+            id: "asadfdsasf",
+            name: "item2",
+            duration: 300
+        }
+    ]
+    res.status(200).send(items);
 }
 
 export const practiceRouter = express.Router();
