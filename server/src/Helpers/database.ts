e293192e04cc645d04e5e468395eb53f2c239cb3
jacekgawron98 <gawron.jacek98@gmail.com>
2021-11-04 19:13:13 +0100

    Change collections validation

--- server/src/Helpers/database.ts
+++ server/src/Helpers/database.ts
@@ -15,7 +15,7 @@ export const connectToDatabase = async () => {
     }
     const db = client.db(config.database_name);
     addItemsValidation(db);
-    // addSetsValidation(db);
+    addSetsValidation(db);
     database = db
     console.log(`[log] Connected to the database ${config.database_name}`)
 }
@@ -25,109 +25,121 @@ export const getCollection = async <T>(collection: string) => {
 }
 
 const addItemsValidation = async (db: mongodb.Db) => {
-    await db.command({
-        "collMod": "items",
-        "validator": {
-            $jsonSchema: {
-                bsonType: "object",
-                required: ["name", "ownerId"],
-                additionalProperties: false,
-                properties: {
-                    _id: {},
-                    name: {
-                        bsonType: "string"
-                    },
-                    description:{
-                        bsonType: "string"
-                    },
-                    duration: {
-                        bsonType: "number"
-                    },
-                    repeats: {
-                        bsonType: "number"
-                    },
-                    statisticName: {
-                        bsonType: "string"
-                    },
-                    videoLink: {
-                        bsonType: "string"
-                    },
-                    ownerId: {
-                        bsonType: "string"
-                    },
+    db.createCollection("items", {}, async (error,collection) => {
+        if (error) {
+            console.log(`[log]: ${error.message}`);
+            return;
+        }
+        await db.command({
+            "collMod": "items",
+            "validator": {
+                $jsonSchema: {
+                    bsonType: "object",
+                    required: ["name", "ownerId"],
+                    additionalProperties: false,
+                    properties: {
+                        _id: {},
+                        name: {
+                            bsonType: "string"
+                        },
+                        description:{
+                            bsonType: "string"
+                        },
+                        duration: {
+                            bsonType: "number"
+                        },
+                        repeats: {
+                            bsonType: "number"
+                        },
+                        statisticName: {
+                            bsonType: "string"
+                        },
+                        videoLink: {
+                            bsonType: "string"
+                        },
+                        ownerId: {
+                            bsonType: "string"
+                        },
+                    }
                 }
             }
-        }
+        })
     })
 }
 
 const addSetsValidation = async (db: mongodb.Db) => {
-    await db.command({
-        "collMod": "sets",
-        "validator": {
-            $jsonSchema: {
-                bsonType: "object",
-                required: ["name", "ownerId", "items"],
-                additionalProperties: false,
-                properties: {
-                    _id: {},
-                    name: {
-                        bsonType: "string"
-                    },
-                    items:{
-                        bsonType: "array",
-                        items: {
+    db.createCollection("sets", {}, async (error,collection) => {
+        if (error) {
+            console.log(`[log]: ${error.message}`);
+            return;
+        }
+        await db.command({
+            "collMod": "sets",
+            "validator": {
+                $jsonSchema: {
+                    bsonType: "object",
+                    required: ["name", "ownerId", "items"],
+                    additionalProperties: false,
+                    properties: {
+                        _id: {},
+                        name: {
+                            bsonType: "string"
+                        },
+                        items:{
+                            bsonType: "array",
+                            items: {
+                                bsonType: "object",
+                                required: ["name", "ownerId"],
+                                additionalProperties: false,
+                                properties: {
+                                    _id: {},
+                                    name: {
+                                        bsonType: "string"
+                                    },
+                                    description:{
+                                        bsonType: "string"
+                                    },
+                                    duration: {
+                                        bsonType: "number"
+                                    },
+                                    repeats: {
+                                        bsonType: "number"
+                                    },
+                                    statisticName: {
+                                        bsonType: "string"
+                                    },
+                                    videoLink: {
+                                        bsonType: "string"
+                                    },
+                                    ownerId: {
+                                        bsonType: "string"
+                                    },
+                                }
+                            }
+                        },
+                        plannedTime: {
                             bsonType: "object",
-                            required: ["name", "ownerId"],
-                            additionalProperties: false,
                             properties: {
-                                _id: {},
-                                name: {
+                                day: {
                                     bsonType: "string"
                                 },
-                                description:{
-                                    bsonType: "string"
-                                },
-                                duration: {
+                                hour: {
                                     bsonType: "number"
                                 },
-                                repeats: {
+                                minute: {
                                     bsonType: "number"
                                 },
-                                statisticName: {
-                                    bsonType: "string"
-                                },
-                                videoLink: {
-                                    bsonType: "string"
-                                },
-                                ownerId: {
-                                    bsonType: "string"
-                                },
+                                isWeekly: {
+                                    bsonType: "bool"
+                                }
                             }
-                        }
-                    },
-                    plannedTime: {
-                        bsonType: "object",
-                        properties: {
-                            day: {
-                                bsonType: "string"
-                            },
-                            hour: {
-                                bsonType: "number"
-                            },
-                            minute: {
-                                bsonType: "number"
-                            },
-                            isWeekly: {
-                                bsonType: "bool"
-                            }
-                        }
-                    },
-                    ownerId: {
-                        bsonType: "string"
-                    },
+                        },
+                        ownerId: {
+                            bsonType: "string"
+                        },
+                    }
                 }
             }
-        }
+        })
     })
 }
